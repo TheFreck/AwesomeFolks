e2f97e9e04df068c1e419699cdaca00756b2943c
Wilhelm Picaco <wpicaco@gmail.com>
2018-11-14 19:33:11 -0700

    curse you Travis!

--- models/users.js
+++ models/users.js
@@ -1,17 +1,22 @@
 module.exports = function(sequelize, DataTypes) {
-    var Users = sequelize.define("users", {
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false
-        }
-    });
-    return Users;
+  var Users = sequelize.define("user", {
+    item: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true
+      }
+    },
+    name: {
+      type: DataTypes.STRING
+    },
+    password: {
+      type: DataTypes.STRING
+    }
+  });
+  return Users;
 };
