2981e1cd09d3c71041b71c2be2c1264d5378db8b
arohadobson <aroha.m.dobson@gmail.com>
2018-11-19 15:53:55 -0700

    updated nav bar and sass

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,77 +1,33 @@
-$(".open-overlay").click(function() {
-  $(".open-overlay").css("pointer-events", "none");
-  var overlay_navigation = $(".overlay-navigation"),
-    top_bar = $(".bar-top"),
-    middle_bar = $(".bar-middle"),
-    bottom_bar = $(".bar-bottom");
+// select DOM Items
+const menuBtn = document.querySelector ('.menu-btn');
+const menu = document.querySelector ('.menu');
+const menuNav = document.querySelector ('.menu-nav');
+const menuBranding = document.querySelector ('.menu-branding');
+const navItem = document.querySelectorAll ('.nav-item');
 
-  overlay_navigation.toggleClass("overlay-active");
-  if (overlay_navigation.hasClass("overlay-active")) {
-    top_bar.removeClass("animate-out-top-bar").addClass("animate-top-bar");
-    middle_bar
-      .removeClass("animate-out-middle-bar")
-      .addClass("animate-middle-bar");
-    bottom_bar
-      .removeClass("animate-out-bottom-bar")
-      .addClass("animate-bottom-bar");
-    overlay_navigation
-      .removeClass("overlay-slide-up")
-      .addClass("overlay-slide-down");
-    overlay_navigation.velocity("transition.slideLeftIn", {
-      duration: 300,
-      delay: 0,
-      begin: function() {
-        $("nav ul li").velocity("transition.perspectiveLeftIn", {
-          stagger: 150,
-          delay: 0,
-          complete: function() {
-            $("nav ul li a").velocity(
-              {
-                opacity: [1, 0]
-              },
-              {
-                delay: 10,
-                duration: 140
-              }
-            );
-            $(".open-overlay").css("pointer-events", "auto");
-          }
-        });
-      }
-    });
-  } else {
-    $(".open-overlay").css("pointer-events", "none");
-    top_bar.removeClass("animate-top-bar").addClass("animate-out-top-bar");
-    middle_bar
-      .removeClass("animate-middle-bar")
-      .addClass("animate-out-middle-bar");
-    bottom_bar
-      .removeClass("animate-bottom-bar")
-      .addClass("animate-out-bottom-bar");
-    overlay_navigation
-      .removeClass("overlay-slide-down")
-      .addClass("overlay-slide-up");
-    $("nav ul li").velocity("transition.perspectiveRightOut", {
-      stagger: 150,
-      delay: 0,
-      complete: function() {
-        overlay_navigation.velocity("transition.fadeOut", {
-          delay: 0,
-          duration: 300,
-          complete: function() {
-            $("nav ul li a").velocity(
-              {
-                opacity: [0, 1]
-              },
-              {
-                delay: 0,
-                duration: 50
-              }
-            );
-            $(".open-overlay").css("pointer-events", "auto");
-          }
-        });
-      }
-    });
-  }
-});
+// Initial state of the menu prior to selecting nav button
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu (){
+	if(!showMenu){
+		menuBtn.classList.add('close');
+		menu.classList.add('show');
+		menuNav.classList.add('show');
+		menuBranding.classList.add('show');
+		navItem.forEach(item => item.classList.add('show'));
+
+		// Menu State: Open
+		showMenu = true;
+	} else {
+		menuBtn.classList.remove('close');
+		menu.classList.remove('show');
+		menuNav.classList.remove('show');
+		menuBranding.classList.remove('show');
+		navItem.forEach(item=> item.classList.remove('show'));
+
+		// Menu State: Closed
+		showMenu = false;
+	}
+}
